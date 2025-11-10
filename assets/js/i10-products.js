38aeb88c9d6cb1ed2dafe2d35a32d51554c5bd3e
i10store <uihvn92@gmail.com>
2025-11-10 23:09:45 +0700

    push

--- assets/js/i10-products.js
+++ assets/js/i10-products.js
@@ -7,7 +7,7 @@
    ========================================================= */
 
 /* ========== CONFIG ========== */
-const SHEET_API = "https://script.google.com/macros/s/AKfycbx2GwR1G8xSY9t8Oq_stEevW5XWp14ASL7SR41J_eT3hU8Eat7vrFsGriT5C7B7-fuE0Q/exec"; // <== Chỉ dùng cho Gửi Form
+const SHEET_API = "https://script.google.com/macros/s/AKfycbxeIGgSYDEMS0f6EVkcKHvyHaXFWQyEB2vK_XO1olIvk8XPN8neiz71-K19DYgiBrFeOA/exec"; // <== Chỉ dùng cho Gửi Form
 const SITE_LOGO = "https://lh3.googleusercontent.com/d/1kICZAlJ_eXq4ZfD5QeN0xXGf9lx7v1Vi=s1000"; 
 const SITE_LOGO_2 = "https://lh3.googleusercontent.com/d/1L6aVgYahuAz1SyzFlifSUTNvmgFIZeft=s1000";
 const THEME = "#76b500";
@@ -139,10 +139,11 @@ async function getProductData() {
                 p["Model"] || p["Name"],
                 p["CPU"],
                 p["RAM"],
+                p["SSD"],
                 p["RESOLUTION"],
                 p["GPU"]
             ].filter(Boolean).join(' ');
-            p.slug = p["Web Link"] || `san-pham/${createSlug(slugText || `product-${i}`)}`;
+            p.slug = p["Web Link"];
         });
         
         globalProductData = data;
